a3043507decdb0ecc9447a7378d82bc7711baeb7
Priyanshu Tripathi <vibecoder144@gmail.com>
2026-06-20 10:19:58 +0530

    add a field

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -13,6 +13,10 @@ class Projects(models.Model):
     image = models.ImageField(upload_to='media/images/')
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=200)
+    links = models.URLField(max_length=500 ,blank=True, default=None)
 
     def __str__(self):
         return self.title
+
+class Tools(models.Model):
+    name = models.CharField(max_length=50)
